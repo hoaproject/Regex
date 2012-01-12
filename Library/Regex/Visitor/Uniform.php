b7e7443f283c673c8ced5c550af2da11bb1c6e1e
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2012-01-12 12:59:13 +0100

    Revert capturing behavior.

--- Library/Regex/Visitor/Uniform.php
+++ Library/Regex/Visitor/Uniform.php
@@ -179,11 +179,11 @@ class Uniform implements \Hoa\Visitor\Visit {
               break;
 
             case '#concatenation':
-                $out = null;
-                $Γ   = $data['precompute'][$n]['Γ'];
-                $γ   = $Γ[$this->_sampler->getInteger(0, count($Γ) - 1)];
+                $out      = null;
+                $Γ        = $data['precompute'][$n]['Γ'];
+                $γ        = $Γ[$this->_sampler->getInteger(0, count($Γ) - 1)];
 
-                foreach($element->getChildren() as $i => $child)
+                foreach($element->getChildre() as $i => $child)
                     $out .= $child->accept($this, $handle, $γ[$i]);
 
                 return $out;
