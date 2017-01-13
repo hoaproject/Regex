d845bfca783f8241a9d10663992d7250f189e410
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-01-13 17:09:09 +0100

    Quality: Happy new year!

--- Grammar.pp
+++ Grammar.pp
@@ -6,7 +6,7 @@
 //
 // New BSD License
 //
-// Copyright © 2007-2016, Hoa community. All rights reserved.
+// Copyright © 2007-2017, Hoa community. All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
@@ -37,7 +37,7 @@
 // parser. More informations at http://pcre.org/pcre.txt, sections pcrepattern &
 // pcresyntax.
 //
-// @copyright  Copyright © 2007-2016 Hoa community.
+// @copyright  Copyright © 2007-2017 Hoa community.
 // @license    New BSD License
 //
 
